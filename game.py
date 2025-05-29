d82a4d6d2f03c1545db0dcc1f876cc7fb4f730fe
Hyeon2 <97961602+hyeon2006@users.noreply.github.com>
2025-05-29 20:06:55 +0900

    Add component

--- game.py
+++ game.py
@@ -4,13 +4,39 @@ white, black, red, blue = (255, 255, 255), (0, 0, 0), (255, 0, 0), (0, 0, 255)
 
 pygame.init()
 
-width, height = 685, 800
+width, height = 305, 400
 screen = pygame.display.set_mode((width, height))
+
+ball = pygame.Rect(width // 2 - 5, height // 2, 10, 10)
+peddle = pygame.Rect(width // 2 - 50, height - 50, 100, 10)
+
+brickRows, brickCols = 3, 4
+brickGap = 5
+brickWidth = 70
+brickHeight = 30
+bricks = []
+for r in range(brickRows):
+    for c in range(brickCols):
+        brick = pygame.Rect(
+            c * (brickWidth + brickGap) + brickGap,
+            r * (brickHeight + brickGap) + brickGap,
+            brickWidth,
+            brickHeight,
+        )
+        bricks.append(brick)
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    screen.fill(white)
+    pygame.draw.ellipse(screen, black, ball)
+    pygame.draw.rect(screen, blue, peddle)
+    for brick in bricks:
+        pygame.draw.rect(screen, red, brick)
+
+    pygame.display.flip()
 
 pygame.quit
