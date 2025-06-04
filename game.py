3a0882eb2e78a95b604958e344f26df02cc8b74f
Hyeon2 <97961602+hyeon2006@users.noreply.github.com>
2025-06-04 19:47:25 +0900

    Feature implementation.

--- game.py
+++ game.py
@@ -8,7 +8,10 @@ width, height = 305, 400
 screen = pygame.display.set_mode((width, height))
 
 ball = pygame.Rect(width // 2 - 5, height // 2, 10, 10)
-peddle = pygame.Rect(width // 2 - 50, height - 50, 100, 10)
+
+ballDx, ballDy = 3, -3
+
+paddle = pygame.Rect(width // 2 - 50, height - 50, 100, 10)
 
 brickRows, brickCols = 3, 4
 brickGap = 5
@@ -31,12 +34,42 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
+    if ball.left <= 0 or ball.right >= width:
+        ballDx = -ballDx
+
+    elif ball.top <= 0:
+        ballDy = -ballDy
+
+    elif ball.bottom >= height:
+        running = False
+
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT] and paddle.left > 0:
+        paddle = paddle.move(-3, 0)
+
+    elif keys[pygame.K_RIGHT] and paddle.right < width:
+        paddle = paddle.move(3, 0)
+
+    ball = ball.move(ballDx, ballDy)
+
+    if ball.colliderect(paddle) and ballDy > 0:
+        ballDy = -ballDy
+
+    for brick in bricks:
+        if ball.colliderect(brick):
+            ballDy = -ballDy
+            bricks.remove(brick)
+    if bricks == []:
+        running = False
+
     screen.fill(white)
     pygame.draw.ellipse(screen, black, ball)
-    pygame.draw.rect(screen, blue, peddle)
+    pygame.draw.rect(screen, blue, paddle)
     for brick in bricks:
         pygame.draw.rect(screen, red, brick)
 
     pygame.display.flip()
+    pygame.time.Clock().tick(60)
 
 pygame.quit
